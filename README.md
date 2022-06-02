ffd455e2bb69f46c737a4ade5a9ece89c236584e
troat9824 <troat9824@gmail.com>
2022-06-02 03:32:43 -0500

    server.js

---
+++ README.md
@@ -0,0 +1,34 @@
+  # Bookish
+  
+  ## Table of Contents
+  
+
+-[Description](#description)
+
+-[Installation](#Installation)
+
+-[Usage](#Usage)
+
+-[Contributions](#Contributions)
+
+-[Questions](#Questions)
+
+  ## Description 
+
+
+  
+  ## Installation 
+    No installation needed! Just follow the following link.
+
+
+
+  ## Usage 
+  Instructions for use:
+  Once opened in the browser, users can search for books and results shall appear. 
+  
+  ## Contributions 
+  Made by Taylor Vaughn 
+
+  ## Questions 
+  Any questions please reach out to me at my [Github](https://github.com/troat9824)
+   or by my email at  troat9824@gmail.com
--- server/server.js
+++ server/server.js
@@ -1,21 +1,45 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
-app.use(routes);
+  // if in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+  }
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+// call function to start server
+startApolloServer(typeDefs, resolvers);
